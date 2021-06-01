0aab14a95b3f9e25a8500cb55308bb0d7efdb763
AlanLoveCod1ng <71209736+AlanLoveCod1ng@users.noreply.github.com>
2021-06-01 15:02:04 +0800

    Update MainClass.java

--- MetaheuristicForTSP/HillClimbing/MainClass.java
+++ MetaheuristicForTSP/HillClimbing/MainClass.java
@@ -1,22 +1,28 @@
-
 public class MainClass {
 
 	public static void main(String[] args) {
-		int nbCities = 10;
-		String fileName = "C://Users//18301//Desktop//File//CPS 3410//code//Assignment//CPS3410AS//src//distances_between_cities_10.txt";
+		int nbCities = 50;
+		String fileName = "C://Users//18301//Desktop//File//CPS 3410//code//Assignment//CPS3410AS//src//distances_entre_villes_50.txt";
 
 		Problem pb = new Problem(nbCities, fileName);
 		pb.constructDistances();
-		pb.printDistances();
+		//pb.printDistances();
 
 		Solution sol = new Solution(pb);
 		OPT opt = new OPT(sol);
 		opt.generateOPT();
+		long t1 = System.currentTimeMillis();
+		int sumFitness = 0;
 		for (int i = 0; i < 10; i++) {
 			opt.optimal = new Solution(pb);
 			opt.generateOPT();
+			opt.min();
+			sumFitness+=opt.optimal.fitness;
 		}
-		opt.min();
+		long t2 = System.currentTimeMillis();
+		t2 -= t1;
+		sumFitness = sumFitness/10;
+		System.out.println("Iterating 10 times cost "+t2+" milliseconds, and average fitness is "+sumFitness);
 	}
 
 }
