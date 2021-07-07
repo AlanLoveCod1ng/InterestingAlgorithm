6d659bcfd522cfb575e5c9d006f612a989dae53d
Alan Hu <71209736+AlanLoveCod1ng@users.noreply.github.com>
2021-07-07 16:23:43 +0800

    Add files via upload

---
+++ MultiobjectiveOptimization/Weighted/VNS/MainClass.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import jxl.*;
+import jxl.write.*;
+import java.io.*;
+public class MainClass {
+
+	public static void main(String[] args) throws Exception{
+		int nbCities =50;
+		String mediumFileName = "C://Users//18301//Desktop//File//CPS 3410//code//Assignment//CPS3410AS//src//dataset//objective1.txt";
+		String lowerFileName = "C://Users//18301//Desktop//File//CPS 3410//code//Assignment//CPS3410AS//src//dataset//objective2.txt";
+//		String upperFileName = "C://Users//18301//Desktop//File//CPS 3410//code//Assignment//CPS3410AS//src//upperdistances_entre_villes_50.txt";
+		Problem pb = new Problem(nbCities, mediumFileName, /*upperFileName,*/ lowerFileName);
+		pb.constructDistances();
+
+		File file = new File("localSearch.xls");
+		file.createNewFile();
+		WritableWorkbook workbook=Workbook.createWorkbook(file);
+		WritableSheet sheet = workbook.createSheet("Alan", 0);
+		Label label;
+
+
+		for(int i = 0; i<=100; i+=10){
+			OPT opt = new OPT(pb, (double)(i)/100, (double)(100-i)/100);
+			opt.optimal.printSolution();
+			label = new Label(0, i/10, opt.optimal.fitnessObj1+"");
+			sheet.addCell(label);
+			label = new Label(1, i/10, opt.optimal.fitnessObj2+"");
+			sheet.addCell(label);
+		}
+		workbook.write();
+		workbook.close();
+	}
+
+}
